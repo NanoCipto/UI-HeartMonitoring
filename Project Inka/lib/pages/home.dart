e249a141916f9570b958a5a9d82bdb87e6de5b57
NanoCipto <nanociptoo@gmail.com>
2024-10-06 20:15:23 +0700

    first commit

---
+++ Project Inka/lib/pages/home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(94, 169, 246, 1),
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+          decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+      body: Stack(
+        children: [
+          // Background SVG yang responsif
+          Positioned.fill(
+            child: SvgPicture.asset(
+              'assets/icons/background.svg',
+              fit: BoxFit.cover, // Agar gambar menyesuaikan dengan layar
+            ),
+          ),
+          // Tombol Back di atas background
+          Positioned(
+            bottom: 80, // Atur jarak tombol dari bawah layar
+            left: 0,
+            right: 0,
+            child: Center(
+              child : ElevatedButton(
+              onPressed: () {
+                // Aksi ketika tombol Back ditekan
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white, // Warna latar tombol
+                foregroundColor: Colors.black, // Warna teks tombol
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 12), // Ukuran padding
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8), // Border persegi dengan sudut melengkung
+                ),
+              ),
+              child: Text(
+                'Back',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 0, 61, 111)),
+              ),
+            ),
+          ),
+        ),
+        ],
+      ),
+    );
+  }
+}
+
+//Coba
